4714808d2c41ba2442b5de3a9bc721e0345a02a2
Abdellatif Zouliga <zuuliga@gmail.com>
2024-05-03 11:34:36 -0400

    Project created

---
+++ src/main/java/com/cydeo/accountingprojectapi/AccountingProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.accountingprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingProjectApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingProjectApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/accountingprojectapi/AccountingProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.accountingprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingProjectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
